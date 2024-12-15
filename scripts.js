b323052e65aba152e8d09c15cc0104b4f730936f
Angelaru87 <mesp222@gmail.com>
2024-12-15 20:17:49 -0300

    Repo

---
+++ scripts.js
@@ -0,0 +1,19 @@
+document.getElementById('Liquidity On').addEventListener('click', function() {
+    alert('¡Botón 1 clicado!');
+});
+
+document.getElementById('Liquidity Off').addEventListener('click', function() {
+    alert('¡Botón 2 clicado!');
+});
+
+document.getElementById('TokAxB').addEventListener('click', function() {
+    alert('¡Botón 3 clicado!');
+});
+
+document.getElementById('TokBxA').addEventListener('click', function() {
+    alert('¡Botón 4 clicado!');
+});
+
+document.getElementById('Get Price').addEventListener('click', function() {
+    alert('¡Botón 5 clicado!');
+});
